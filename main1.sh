988e6a101994150bc5c9482b17e4856eebaea2d0
Almer Rashad <almerrashad003@gmail.com>
2023-09-03 20:33:09 +0700

    Soal 1

---
+++ main1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+path="/hdfs/data/data1"
+name_of_directory="data1"
+
+if [ -d "$path/$name_of_directory" ]; then
+  echo "There is $name_of_directory Directory Exists!"
+else
+  echo "$name_of_directory Directory Not Exists!"
+  mkdir -p "$path/$name_of_directory"
